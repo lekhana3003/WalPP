b80f7dcfc563911657315fb041142c8cde7ddb85
Lekhana G <lekhana.g@perfios.com>
2021-07-02 00:29:27 +0530

    intial setuo

---
+++ src/main/java/com/example/WalPP/WalPpApplication.java
@@ -0,0 +1,13 @@
+package com.example.WalPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalPpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalPpApplication.class, args);
+	}
+
+}

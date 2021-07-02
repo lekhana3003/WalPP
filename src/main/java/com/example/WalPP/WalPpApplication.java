182ebbae3dcc8513e3c7d0d9da7efc11bfc7ebc3
Lekhana G <lekhana.g@perfios.com>
2021-07-02 20:08:37 +0530

    added swagger UI

--- src/main/java/com/example/WalPP/WalPpApplication.java
+++ src/main/java/com/example/WalPP/WalPpApplication.java
@@ -2,12 +2,20 @@ package com.example.WalPP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class WalPpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WalPpApplication.class, args);
 	}
-
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.WalPP")).build();
+	}
 }
